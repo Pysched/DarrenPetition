121f5e9ad50e8af2d747d879d543eed20611f304
Pysched <48335713+Pysched@users.noreply.github.com>
2023-11-04 00:56:25 +0000

    Created create.html with form to capture a petition title and submit button Updated the index.html page to display a message if there are no petitions available, otherwise for each title, display it in a list item at present. Updated the controller to add in mapping for the create petition page Created a process create petition process that takes the paramaeters adds them to a list for calling back on the index page.

--- src/main/java/com/example/DarrensPetitions/controllers/PetitionController.java
+++ src/main/java/com/example/DarrensPetitions/controllers/PetitionController.java
@@ -3,13 +3,36 @@ package com.example.DarrensPetitions.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 
 public class PetitionController {
+    static List<String> petitions = new ArrayList<>();
+
     @RequestMapping("/")
     public String index(Model model) {
         model.addAttribute("title", "Darren's Petitions");
+        model.addAttribute("petitions", petitions);
         return "index";
     }
+
+    @RequestMapping(value="/create", method= RequestMethod.GET)
+    public String createPetition(Model model) {
+        model.addAttribute("title", "Create a Petition");
+        return "create";
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public String processCreatePetition(
+            @RequestParam String petitionTitle) {
+        petitions.add(petitionTitle);
+
+        return "redirect:/";
+    }
+
 }
